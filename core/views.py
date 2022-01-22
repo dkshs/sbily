7d1e6fdd74537745613b2cd8808b93a4a5c0a60b
DKsHs <nicolas.contiero198@gmail.com>
2022-01-21 22:37:28 -0300

    Modificando os erros 404/500

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import FormLinks
 from .models import Links
+from django.template import RequestContext
 
 def home(request):
     form = FormLinks()
@@ -30,3 +31,13 @@ def redirecionar(request, link):
         return redirect('/')
 
     return redirect(link[0].link_redirecionado)
+
+def handler404(request, exception, template_name="404.html"):
+    response = render(request, template_name)
+    response.status_code = 404
+    return response
+
+def handler500(request, *args, **argv):
+    response = render(request, '500.html')
+    response.status_code = 500
+    return response
--- encurtador_links/urls.py
+++ encurtador_links/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
+handler404 = 'core.views.handler404'
+handler500 = 'core.views.handler500'
