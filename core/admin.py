b5bb4950ddccbf36499d00c55e9679944b00a16f
DKSHS <nicolas.contiero198@gmail.com>
2022-07-18 18:00:40 -0300

    Formatting codes

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Link
 
+
 @admin.register(Link)
 class LinksADM(admin.ModelAdmin):
-    list_display = ('link_redirecionado', 'link_fixo', 'link_encurtado')
-    search_fields = ['link_redirecionado', 'link_encurtado']
+    list_display = ("link_redirecionado", "link_fixo", "link_encurtado")
+    search_fields = ["link_redirecionado", "link_encurtado"]
--- core/models.py
+++ core/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from decouple import config
 
+
 class Link(models.Model):
     link_redirecionado = models.URLField()
     link_encurtado = models.CharField(max_length=8, unique=True)
-    link_fixo = config("DOMAIN_DEFAULT", default="http://127.0.0.1:8000/") 
+    link_fixo = config("DOMAIN_DEFAULT", default="http://127.0.0.1:8000/")
 
     def __str__(self):
         return self.link_encurtado
