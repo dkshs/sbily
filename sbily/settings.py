74719d70fe63e70564e63c0442a23d5614b275b6
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2022-11-30 12:49:39 -0300

    Fix: Bug allowed hosts

--- sbily/settings.py
+++ sbily/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = [config("DEFAULT_DOMAIN", default="http://127.0.0.1:8000/")]
+ALLOWED_HOSTS = []
 
 
 # Application definition
