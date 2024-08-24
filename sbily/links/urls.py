4df11db8ed6d4da01a00a31f4c110caf911e1d0d
dkshs <88355080+dkshs@users.noreply.github.com>
2024-08-24 19:11:07 -0300

    refactor: move the update link function to another url and view

--- sbily/links/urls.py
+++ sbily/links/urls.py
@@ -5,12 +5,13 @@ from . import views
 
 link_urlpatterns = [
     path("", views.link, name="link"),
+    path("update", views.update_link, name="update_link"),
     path("delete", views.delete_link, name="delete_link"),
 ]
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("create_link/", views.create_link, name="create_link"),
-    path("link/<str:shortened_link>/", include(link_urlpatterns)),
     path("<str:shortened_link>/", views.redirect_link, name="redirect_link"),
+    path("link/<str:shortened_link>/", include(link_urlpatterns)),
 ]
--- sbily/links/views.py
+++ sbily/links/views.py
@@ -1,10 +1,13 @@
 # ruff: noqa: BLE001
+from datetime import UTC
+from datetime import datetime
+from datetime import timedelta
+
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.shortcuts import render
-from datetime import datetime, timedelta
 
 from .models import ShortenedLink
 from .utils import can_user_create_link
@@ -16,50 +19,6 @@ BASE_URL = getattr(settings, "BASE_URL", None)
 @login_required
 def home(request):
     return render(request, "home.html", {"BASE_URL": BASE_URL})
-
-
-@login_required
-def link(request, shortened_link):
-    try:
-        link = ShortenedLink.objects.get(shortened_link=shortened_link)
-        if request.method == "POST":
-            original_link = request.POST.get("original_link") or ""
-            shortened_link = request.POST.get("shortened_link") or ""
-            is_active = request.POST.get("is_active") == "on" or False
-            is_temporary = request.POST.get("is_temporary") == "on" or False
-            if not link_is_valid(request, original_link, shortened_link, link.id):
-                return redirect("link", link.shortened_link)
-            if (
-                original_link == link.original_link
-                and shortened_link == link.shortened_link
-                and (is_active and link.is_active)
-                and (is_temporary and link.remove_at is not None)
-            ):
-                messages.warning(request, "No changes were made")
-                return redirect("link", link.shortened_link)
-            link.original_link = original_link
-            link.shortened_link = shortened_link
-            link.is_active = is_active
-            link.updated_at.now()
-            link.remove_at = datetime.now() + timedelta(days=1) if is_temporary else None
-            link.save()
-            messages.success(request, "Link updated successfully")
-
-        deactivate = request.GET.get("deactivate")
-        if deactivate is not None:
-            deactivate = deactivate == "True" or False
-            link.is_active = not deactivate
-            link.save()
-            messages.success(request, f"Link {'deactivated' if deactivate else 'activated'}")
-            return redirect("my_account")
-
-        return render(request, "link.html", {"link": link, "BASE_URL": BASE_URL})
-    except ShortenedLink.DoesNotExist:
-        messages.error(request, "Link not found")
-        return redirect("home")
-    except Exception:
-        messages.error(request, "An error occurred")
-        return redirect("home")
 
 
 @login_required
@@ -76,7 +35,7 @@ def create_link(request):
             original_link=original_link,
             shortened_link=shortened_link,
             user=request.user,
-            remove_at=datetime.now() + timedelta(days=1) if is_temporary else None,
+            remove_at=datetime.now(UTC) + timedelta(days=1) if is_temporary else None,
         )
         messages.success(request, "Link created successfully")
         return redirect("link", shortened_link=shortened_link)
@@ -103,6 +62,78 @@ def redirect_link(request, shortened_link):
 
 
 @login_required
+def link(request, shortened_link):
+    try:
+        link = ShortenedLink.objects.get(
+            shortened_link=shortened_link,
+            user=request.user,
+        )
+
+        deactivate = request.GET.get("deactivate")
+        if deactivate is not None:
+            deactivate = deactivate == "True" or False
+            link.is_active = not deactivate
+            link.save()
+            messages.success(
+                request,
+                f"Link {'deactivated' if deactivate else 'activated'}",
+            )
+            return redirect("my_account")
+
+        return render(request, "link.html", {"link": link, "BASE_URL": BASE_URL})
+    except ShortenedLink.DoesNotExist:
+        messages.error(request, "Link not found")
+        return redirect("home")
+    except Exception:
+        messages.error(request, "An error occurred")
+        return redirect("home")
+
+
+@login_required
+def update_link(request, shortened_link):
+    try:
+        if request.method != "POST":
+            return redirect("my_account")
+
+        link = ShortenedLink.objects.get(
+            shortened_link=shortened_link,
+            user=request.user,
+        )
+        original_link = request.POST.get("original_link") or ""
+        shortened_link = request.POST.get("shortened_link") or ""
+        is_active = request.POST.get("is_active") == "on" or False
+        is_temporary = request.POST.get("is_temporary") == "on" or False
+
+        if not link_is_valid(request, original_link, shortened_link, link.id):
+            return redirect("link", link.shortened_link)
+
+        if (
+            original_link == link.original_link
+            and shortened_link == link.shortened_link
+            and (is_active and link.is_active)
+            and (is_temporary and link.remove_at is not None)
+        ):
+            messages.warning(request, "No changes were made")
+            return redirect("link", link.shortened_link)
+
+        link.original_link = original_link
+        link.shortened_link = shortened_link
+        link.is_active = is_active
+        link.updated_at.now()
+        link.remove_at = datetime.now(UTC) + timedelta(days=1) if is_temporary else None
+        link.save()
+
+        messages.success(request, "Link updated successfully")
+        return redirect("link", link.shortened_link)
+    except ShortenedLink.DoesNotExist:
+        messages.error(request, "Link not found")
+        return redirect("my_account")
+    except Exception:
+        messages.error(request, "An error occurred")
+        return redirect("home")
+
+
+@login_required
 def delete_link(request, shortened_link):
     try:
         link = ShortenedLink.objects.get(
@@ -114,7 +145,7 @@ def delete_link(request, shortened_link):
         return redirect("my_account")
     except ShortenedLink.DoesNotExist:
         messages.error(request, "Link not found")
-        return redirect("home")
+        return redirect("my_account")
     except Exception:
         messages.error(request, "An error occurred")
         return redirect("home")
