b57d17285721e0307df9e4935c13edec6bb107b3
dkshs <88355080+dkshs@users.noreply.github.com>
2025-01-27 14:05:39 -0300

    chore: update `cleanup_clocked_schedules` task

--- config/tasks.py
+++ config/tasks.py
@@ -1,23 +1,32 @@
 from celery import shared_task
+from django.utils.timezone import now
+from django.utils.timezone import timedelta
 
 from sbily.utils.tasks import default_task_params
 from sbily.utils.tasks import task_response
 
 
 @shared_task(**default_task_params("cleanup_clocked_schedules", acks_late=True))
-def cleanup_clocked_schedules(self):
+def cleanup_clocked_schedules():
     """
     Clean up expired clocked schedules that are not associated with any periodic tasks.
-    Also removes disabled periodic tasks containing 'Remove link' in their name.
+    Also removes disabled periodic tasks containing 'Remove link' and
+    'Remove deleted link' in their name.
     """
     from django_celery_beat.models import ClockedSchedule
     from django_celery_beat.models import PeriodicTask
 
-    periodic_deleted = PeriodicTask.objects.filter(
+    periodic_tasks = PeriodicTask.objects.filter(
         enabled=False,
-        name__contains="Remove link ",
-    ).delete()[0]
+        name__regex=r"^(Remove link|Remove deleted link)",
+    )
 
+    threshold_time = now() + timedelta(minutes=5)
+    periodic_tasks.filter(clocked__clocked_time__gte=threshold_time).update(
+        enabled=True,
+    )
+
+    periodic_deleted = periodic_tasks.delete()[0]
     schedule_deleted = ClockedSchedule.objects.filter(
         periodictask__isnull=True,
     ).delete()[0]
